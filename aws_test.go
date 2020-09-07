28b0bdbcbdac5986ce415b712744d08e39e17361
Mike Tougeron <tougeron@adobe.com>
2020-09-07 14:30:54 -0700

    add tests for listEC2InstanceAMIs

--- aws_test.go
+++ aws_test.go
@@ -16,11 +16,12 @@ type mockEC2Client struct {
 	result InstanceAMIsResult
 }
 
-func (m *mockEC2Client) DescribeInstancesPages(*ec2.DescribeInstancesInput, func(*ec2.DescribeInstancesOutput, bool) bool) error {
+func (m *mockEC2Client) DescribeInstancesPages(in *ec2.DescribeInstancesInput, fn func(*ec2.DescribeInstancesOutput, bool) bool) error {
+	fn(&m.resp, true)
 	return nil
 }
 
-func Test_listEC2InstanceAMIs(t *testing.T) {
+func Test_listEC2InstanceAMIsSuccess(t *testing.T) {
 	results := InstanceAMIsResult{}
 	results.InstanceAMIs = append(results.InstanceAMIs, aws.String("ami-123abc"))
 	results.Err = nil
@@ -62,10 +63,56 @@ func Test_listEC2InstanceAMIs(t *testing.T) {
 		var instanceResult InstanceAMIsResult
 		instanceResult = <-e.listEC2InstanceAMIs(instanceIDs)
 
-		// amiIDs := e.listEC2InstanceAMIs(instanceIDs)
-
 		assert := assert.New(t)
 
 		assert.EqualValues(c.result, instanceResult)
+	}
+}
+
+func Test_listEC2InstanceAMIsFail(t *testing.T) {
+	results := InstanceAMIsResult{}
+	results.InstanceAMIs = append(results.InstanceAMIs, aws.String("ami-123abc"))
+	results.Err = nil
+	cases := []mockEC2Client{
+		{
+			resp: ec2.DescribeInstancesOutput{
+				Reservations: []*ec2.Reservation{
+					{
+						Instances: []*ec2.Instance{
+							{
+								ImageId: aws.String("ami-123abc"),
+							},
+							{
+								ImageId: aws.String("ami-456def"),
+							},
+							{
+								ImageId: aws.String("ami-123abc"),
+							},
+						},
+					},
+				},
+			},
+			result: results,
+		},
+	}
+
+	var instanceIDs []*string
+	instanceIDs = append(instanceIDs, aws.String("i-abc123"))
+	instanceIDs = append(instanceIDs, aws.String("i-abc456"))
+	instanceIDs = append(instanceIDs, aws.String("i-abc789"))
+	for _, c := range cases {
+		e := Client{
+			&mockEC2Client{
+				resp:   c.resp,
+				result: c.result,
+			},
+		}
+
+		var instanceResult InstanceAMIsResult
+		instanceResult = <-e.listEC2InstanceAMIs(instanceIDs)
+
+		assert := assert.New(t)
+
+		assert.NotEqual(c.result, instanceResult)
 	}
 }
